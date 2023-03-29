f98bacb9c8039814a56e9f0610e3b1f62e496a1c
Bruno Kobi Valadares de Amorim <38856838+brunokobi@users.noreply.github.com>
2023-03-29 01:57:34 -0300

    chatgpt teste

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,7 @@ import Home from "../pages/Home";
 import About from "../pages/About";
 import Projects from "../pages/Projects";
 import Nav from "../components/Nav";
+import InputComChat from "../pages//ChatGPT//index";
 
 const Router = () => {
   return (
@@ -11,7 +12,8 @@ const Router = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/projects" element={<Projects />} />       
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/chat" element={<InputComChat />} />           
       </Routes>
       <Nav />
     </Box>
