f7134c308a5de5d25da5d146ee2079a6c43a67bd
Bruno Kobi Valadares de Amorim <38856838+brunokobi@users.noreply.github.com>
2023-08-06 01:19:23 -0300

    add contador de visitas

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import { Box,Image,Heading,Stack } from "@chakra-ui/react";
+import { Box,Image,Heading} from "@chakra-ui/react";
 import Home from "../pages/Home";
 import About from "../pages/About";
 import Projects from "../pages/Projects";
