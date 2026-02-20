ee1d4809811648fe26881ba9cb40cbb814bfc21a
Bruno Kobi Valadares de Amorim <38856838+brunokobi@users.noreply.github.com>
2026-02-20 15:10:26 -0300

    icons

--- src/components/IconsLine/index.jsx
+++ src/components/IconsLine/index.jsx
@@ -1,28 +1,25 @@
 import { Flex } from "@chakra-ui/react";
-import { FaPhp } from "react-icons/fa";
-import { FaHtml5 } from "react-icons/fa";
-import { FaNodeJs } from "react-icons/fa";
-import { TbBrandJavascript } from "react-icons/tb"
-import { FaPython } from "react-icons/fa";
-import { FaCss3Alt } from "react-icons/fa";
-import { FaReact } from "react-icons/fa";
-import { FaJava } from "react-icons/fa";
-import { SiTypescript } from "react-icons/si";
-import { SiC } from "react-icons/si";
-import { SiVisualstudiocode } from "react-icons/si";
-import { SiLatex } from "react-icons/si";
-import { SiMysql } from "react-icons/si";
-import { DiProlog } from "react-icons/di";
-import { DiPostgresql } from "react-icons/di";
 
+import { 
+  FaPhp, FaHtml5, FaNodeJs, FaPython, FaCss3Alt, 
+  FaReact, FaJava, FaLaravel, FaCube, FaHeart 
+} from "react-icons/fa";
 
+import { TbBrandJavascript, TbBrandSupabase, TbBrandVscode } from "react-icons/tb";
+
+// Adicionado SiN8N e SiChatwoot oficiais
+import { 
+  SiTypescript, SiC, SiLatex, SiMysql, SiDocker, SiGithub, SiN8N, SiChatwoot 
+} from "react-icons/si";
+
+import { DiProlog, DiPostgresql } from "react-icons/di";
 
 import AnimatedIcon from "../../components/AnimatedIcon";
 
 const IconsLine = () => {
-  const icons = [    
+  const baseIcons = [    
     FaPhp,
-    SiC ,
+    SiC,
     FaHtml5,
     FaCss3Alt,
     TbBrandJavascript,
@@ -31,56 +28,27 @@ const IconsLine = () => {
     FaNodeJs,
     FaPython,
     FaJava,
-    SiVisualstudiocode,
+    TbBrandVscode, 
     SiLatex,
     SiMysql,
     DiProlog,
     DiPostgresql,
-    FaPhp,
-    SiC ,
-    FaHtml5,
-    FaCss3Alt,
-    TbBrandJavascript,
-    SiTypescript,
-    FaReact,
-    FaNodeJs,
-    FaPython,
-    FaJava,
-    SiVisualstudiocode,
-    SiLatex,
-    SiMysql,
-    DiProlog,
-    DiPostgresql,
-    FaPhp,
-    SiC ,
-    FaHtml5,
-    FaCss3Alt,
-    TbBrandJavascript,
-    SiTypescript,
-    FaReact,
-    FaNodeJs,
-    FaPython,
-    FaJava,
-    SiVisualstudiocode,
-    SiLatex,
-    SiMysql,
-    DiProlog,
-    DiPostgresql,
-    FaPhp,
-    SiC ,
-    FaHtml5,
-    FaCss3Alt,
-    TbBrandJavascript,
-    SiTypescript,
-    FaReact,
-    FaNodeJs,
-    FaPython,
-    FaJava,
-    SiVisualstudiocode,
-    SiLatex,
-    SiMysql,
-    DiProlog,
-    DiPostgresql,       
+    TbBrandSupabase,
+    FaLaravel,
+    SiN8N,       // Ícone oficial do n8n
+    SiChatwoot,  // Ícone oficial do Chatwoot
+    FaHeart,     // Coração representando o Lovable
+    FaCube,      // Cubo do Three.js
+    SiDocker,
+    SiGithub
+  ];
+
+  const icons = [
+    ...baseIcons,
+    ...baseIcons,
+    ...baseIcons,
+    ...baseIcons,
+    ...baseIcons
   ];
 
   return (
@@ -95,8 +63,7 @@ const IconsLine = () => {
       <Flex>
         {icons.map((icon, i) => (
           <AnimatedIcon
-            reactIcon={icons[Math.floor(Math.random() * 20)]}          
-            // reactIcon={icon}
+            reactIcon={icons[Math.floor(Math.random() * icons.length)]}          
             key={i}
           />
         ))}
