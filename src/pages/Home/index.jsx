6bd35f6cccae367c3d1f7c970b0a49fac621bbba
Bruno <bruno.kobi@vennx.com.br>
2026-06-26 17:00:22 -0300

    fix(home): restaura GIF com transparência original

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -64,7 +64,7 @@ useEffect(() => {
             offsetX="-50%"
             transition="all 1s"
           >
-            <Image src={profile} w={{ base: 250, md: 300 }} style={{ mixBlendMode: "screen" }} />
+            <Image src={profile} w={{ base: 250, md: 300 }} />
             
           </Stack>
 
